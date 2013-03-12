fe4477fe7118d601fca626243f7258b3c0f2f295
aheld <aarondheld@gmail.com>
2013-03-12 19:44:01 -0400

    working w/ mock data

--- 04-freebase-app/app/scripts/controllers/list.js
+++ 04-freebase-app/app/scripts/controllers/list.js
@@ -2,9 +2,18 @@
 
 FreebaseAppApp.controller('ListCtrl', function($scope,freebase,$routeParams) {
   $scope.req = freebase.searchFreeBase($routeParams.query);
+  $scope.ShowDetails = function(item){
+  	item.details = freebase.getItem(item.id); 
+  }
+  $scope.HideDetails = function(item){
+  	item.details = undefined; 
+  }
+
+
 });
 
 
 FreebaseAppApp.controller('ItemCtrl', function($scope,freebase,$routeParams) {
   $scope.req = freebase.getItem($routeParams.prefix + '/' + $routeParams.id);
 });
+
